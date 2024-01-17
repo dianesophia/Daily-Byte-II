3a98391bbe4a8baffdd71f6a9016ecb484868a39
dianesophia <90141802+dianesophia@users.noreply.github.com>
2024-01-18 02:30:55 +0800

    (2) Valid Palindrome

---
+++ ValidPalindrome.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class ValidPalindrome{
+  public static void main(String ...args){
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter a Word: ");
+    String word = sc.nextLine();
+    
+    StringBuilder reverseWord = new StringBuilder(word).reverse();
+    String newWord = reverseWord.toString();
+    
+    boolean ans = (word.equals(newWord)) ? true :  false; 
+     
+    System.out.print(ans);
+    
+    
+        
+  }
+}
