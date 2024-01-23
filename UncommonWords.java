da43d668afbf0486ff2fc08232dd7c2a8967cdf6
dianesophia <90141802+dianesophia@users.noreply.github.com>
2024-01-23 23:55:25 +0800

    (14) Uncommon Words

---
+++ UncommonWords.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class UncommonWords{
+   public static void main(String ...args){
+      Scanner sc = new Scanner(System.in);
+      Map<String, Integer> map1 = new HashMap<>();
+      Map<String, Integer> map2 = new HashMap<>();
+      Set<String> set = new HashSet<>();
+      
+      System.out.print("Enter the first sentence :");
+      String sentence1 = sc.nextLine();
+      String[] s1 = sentence1.split(" ");
+      
+      for(int i = 0; i < s1.length; i++){
+         map1.put(s1[i], i);
+      }
+      
+      System.out.print("Enter the second sentence :");
+      String sentence2 = sc.nextLine();
+      String[] s2 = sentence2.split(" ");
+      
+      for(int i = 0; i < s2.length; i++){
+         map2.put(s2[i], i);
+      }
+      
+      
+      for(int i = 0; i < s1.length; i++){
+         if(!map2.containsKey(s1[i])){
+            set.add(s1[i]);
+         }
+      }
+      
+      for(int i = 0; i < s2.length; i++){
+         if(!map1.containsKey(s2[i])){
+            set.add(s2[i]);
+         }
+      }
+      
+      System.out.print("The uncommon words are : " + set);
+   }
+}
